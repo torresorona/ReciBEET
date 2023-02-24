4359da3ff9124489fff6a49b5518342641e7f087
Hailey Francis <n01402670@unf.edu>
2023-02-23 20:56:12 -0500

    added Recipe.js model, may add more booleans depending on if we want the user to be able to search user created recipes by specific tags

---
+++ models/Recipe.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Recipe extends Model {}
+
+Recipe.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        recipeName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        instructions: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        //example boolean for tags, may need to add more depending on what needs to be saved
+        isGlutenFree: {
+            type: DataTypes.BOOLEAN,
+            //leaving to allow null as default for now
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        views: {
+            type: DataTypes.INTEGER,
+            default: 0,
+        }
+    }
+);
+
+module.exports = Recipe;
