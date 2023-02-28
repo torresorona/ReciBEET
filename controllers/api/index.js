b133f32532e2fd43542b0c7320ffb01db55abf23
Kelsei Kyser <kkelsei127@yahoo.com>
2023-02-27 21:36:22 -0500

    changed recipeRoutes name and added to api/index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const recipeRoutes = require('./recipeRoutes');
+const spoonacularRoutes = require('./spoonacularRoutes');
 
 router.use('/users', userRoutes);
 router.use('/recipe', recipeRoutes);
+router.use('/spoonacularRoutes', spoonacularRoutes);
 
 module.exports = router;
