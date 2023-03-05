c5503c962266db4c1c06b3dc246ade3f16778dd5
shobannah <116910257+shobannah@users.noreply.github.com>
2023-03-04 20:19:09 -0500

    created buttons for adding ingredient

---
+++ public/js/createingredientbtn.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    let next = 1;
+    $(".add-more").click(function(e){
+        e.preventDefault();
+        let addto = "#field" + next;
+        let addRemove = "#field" + (next);
+        next = next + 1;
+        let newIn = '<input autocomplete="off" class="ingredient input form-control" id="field' + next + '" name="field' + next + '" type="text" placeholder="INGREDIENT">';
+        let newInput = $(newIn);
+        let removeBtn = '<button id="remove' + (next - 1) + '" class="btn btn-danger remove-me" >-</button></div><div id="field">';
+        let removeButton = $(removeBtn);
+        $(addto).after(newInput);
+        $(addRemove).after(removeButton);
+        $("#field" + next).attr('data-source',$(addto).attr('data-source'));
+        $("#count").val(next);  
+        
+            $('.remove-me').click(function(e){
+                e.preventDefault();
+                const fieldNum = this.id.charAt(this.id.length-1);
+                const fieldID = "#field" + fieldNum;
+                $(this).remove();
+                $(fieldID).remove();
+            });
+    });
+        
+});
